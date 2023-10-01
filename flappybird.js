43cc1fae36f698c74c014cdc747af8ceaa2cd1e3
Sheriff30 <Sherifdevv@gmail.com>
2023-10-01 17:55:03 +0100

    first commit

---
+++ flappybird.js
@@ -0,0 +1,154 @@
+// board
+
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+// bird
+
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let movingPipe = -2;
+let movingBird = 0;
+let gravity = 0.4;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+let score = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+let gameOver = false;
+
+window.onload = function () {
+  board = document.querySelector(".board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+
+  context = board.getContext("2d");
+
+  birdImg = new Image();
+  birdImg.src = "/flappybird.png";
+
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
+
+  topPipeImg = new Image();
+  topPipeImg.src = "/toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "/bottompipe.png";
+
+  requestAnimationFrame(update);
+  setInterval(placePipes, 1500);
+  document.addEventListener("keydown", movebird);
+  document.addEventListener("touchstart", movebird);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  if (gameOver) {
+    return;
+  }
+  context.clearRect(0, 0, board.width, board.height);
+  movingBird += gravity;
+  bird.y = Math.max(bird.y + movingBird, 0);
+  context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+  if (bird.y > board.height) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += movingPipe;
+    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5;
+      pipe.passed = true;
+    }
+
+    if (logic(bird, pipe)) {
+      gameOver = true;
+    }
+  }
+
+  while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+    pipeArray.shift();
+  }
+
+  context.fillStyle = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+
+  if (gameOver) {
+    context.fillText("GAME OVER", 5, 90);
+  }
+}
+
+function placePipes() {
+  let randomPipY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+  let openSpace = board.height / 4;
+
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+
+  pipeArray.push(topPipe);
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipY + pipeHeight + openSpace,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+
+  pipeArray.push(bottomPipe);
+}
+
+function movebird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    movingBird = -6;
+
+    if (gameOver) {
+      bird.y = birdY;
+      pipeArray = [];
+      score = 0;
+      gameOver = false;
+    }
+  }
+}
+
+function logic(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
